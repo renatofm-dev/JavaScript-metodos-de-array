e6e56c65fb7647285820b0e9113f3fc1104d79a6
Renato Martins <rmartins6694@gmail.com>
2022-10-23 15:04:27 -0300

    add forEach in main.js

--- app/main.js
+++ app/main.js
@@ -2,9 +2,30 @@ let livros = []
 
 const endpointDaAPI = 'https://guilhermeonrails.github.io/casadocodigo/livros.json'
 getBuscarLivrosDaAPI();
+const elementInsertLivros = document.getElementById('livros');
+
 
 async function getBuscarLivrosDaAPI() {
     const res = await fetch(endpointDaAPI)
     livros = await res.json();
     console.table(livros);
+    exibirOsLivroNaTela (livros);
+}
+
+function exibirOsLivroNaTela(listaDeLivros) {
+    listaDeLivros.forEach(livro => {
+        elementInsertLivros.innerHTML += `
+        <div class="livro">
+        <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
+        <h2 class="livro__titulo">
+          ${livro.titulo}
+        </h2>
+        <p class="livro__descricao">${livro.autor}</p>
+        <p class="livro__preco" id="preco">R$ ${livro.preco}</p>
+        <div class="tags">
+          <span class="tag">${livro.categoria}</span>
+        </div>
+      </div>
+        `
+    });
 }
